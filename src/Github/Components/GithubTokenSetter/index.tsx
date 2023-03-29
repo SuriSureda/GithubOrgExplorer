7a588765c509e7053040e1d359b1fa018734879c
Andreu Sureda <suredaps4@gmail.com>
2023-03-29 21:18:31 +0200

    GithubTokenSetter Component

--- src/Github/Components/GithubTokenSetter/index.tsx
+++ src/Github/Components/GithubTokenSetter/index.tsx
@@ -1,13 +1,50 @@
+import { ChangeEvent, useState } from 'react';
 import { Card } from '../../../Shared/Components/Card';
 import { useAppState } from '../../../Shared/Hooks/useAppState';
 import './index.css';
 
+const placeholder = 'Your GITHUB token';
+
 export const GithubTokenSetter = () => {
-  const { updateToken, clearToken } = useAppState();
+  const { github } = useAppState();
+  const { updateToken, clearToken } = github;
+  const [token, setToken] = useState(github.token ?? '');
+
+  const handleChangeToken = (e: ChangeEvent<HTMLInputElement>) => {
+    setToken(e.target.value);
+  };
+
+  const handleClearToken = () => {
+    clearToken();
+    setToken('');
+  };
+
+  const handleSaveToken = () => {
+    updateToken(token);
+  };
 
   return (
     <div id='github-token-setter-container'>
-      <Card>hola</Card>
+      <Card>
+        In order to increase rate limit for Github API calls, provide your Github token here:
+        <br />
+        <input
+          id='github-token-input'
+          type='text'
+          value={token}
+          style={{ width: `${(token?.length < placeholder.length ? placeholder.length : token.length) + 1}ch` }}
+          onChange={handleChangeToken}
+          placeholder={placeholder}
+        />
+        <br />
+        {github.token ? (
+          <button onClick={handleClearToken}>Clear token</button>
+        ) : (
+          <button onClick={handleSaveToken} disabled={!token}>
+            Save token
+          </button>
+        )}
+      </Card>
     </div>
   );
 };
