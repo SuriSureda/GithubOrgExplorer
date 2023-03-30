e8e26428fc16d42039888b39c52d8c788cd47720
Andreu Sureda <suredaps4@gmail.com>
2023-03-30 16:49:23 +0200

    Modal component

---
+++ src/Shared/Components/Modal/index.tsx
@@ -0,0 +1,50 @@
+import { MouseEventHandler, ReactNode, useEffect, useRef } from 'react';
+import { useSpring, animated } from 'react-spring';
+
+import './index.css';
+
+export type ModalProps = {
+  showModal: boolean;
+  onClose: () => void;
+  closeOnClickOutside?: boolean;
+};
+
+type Props = ModalProps & {
+  children: ReactNode;
+};
+
+export const Modal: React.FC<Props> = ({ showModal, onClose, closeOnClickOutside = true, children }) => {
+  const overlayRef = useRef<HTMLDivElement>(null);
+  const animation = useSpring({
+    config: {
+      duration: 250,
+    },
+    opacity: showModal ? 1 : 0,
+    width: showModal ? '100%' : '0%',
+    height: showModal ? '100%' : '0%',
+    overflow: showModal ? 'visible' : 'hidden',
+    display: showModal ? 'flex' : '',
+  });
+
+  const handleClose = () => {
+    onClose();
+  };
+
+  const handleClickOverlay: MouseEventHandler<HTMLDivElement> = (e) => {
+    if (overlayRef.current !== e.target) return;
+    onClose();
+  };
+
+  return (
+    <animated.div style={animation} className='modal-overlay' ref={overlayRef} onClick={closeOnClickOutside ? handleClickOverlay : undefined}>
+      <div className='modal-wrapper'>
+        <div className='modal-header'>
+          <span className='modal-close-icon' onClick={handleClose}>
+            ❌
+          </span>
+        </div>
+        <div className='modal-content'>{children}</div>
+      </div>
+    </animated.div>
+  );
+};
